afae5aeecd5dd8a486eb46c986d8d821c96ce385
Ruslan689 <dovhai.pn@ucu.edu.ua>
2024-10-13 16:29:59 +0300

    ййуккее

--- src/test/java/flower/store/StoreTest.java
+++ src/test/java/flower/store/StoreTest.java
@@ -54,7 +54,6 @@ public class StoreTest {
     /**
      * Tests searching for a non-existing flower in the store.
      */
-    
     @Test
     public void testSearchForNonExistingFlower() {
         Flower rose = new Flower();
